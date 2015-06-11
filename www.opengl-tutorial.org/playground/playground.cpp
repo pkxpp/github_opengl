a6eeec4ac1cd449177a897c0ed77c243b77c45e9
page <pkxpp@126.com>
2015-06-11 13:20:24 +0800

    tutorial 4 matrix
    学习了两个函数：
    （1）perspective
    （2）lookAt

--- www.opengl-tutorial.org/playground/playground.cpp
+++ www.opengl-tutorial.org/playground/playground.cpp
@@ -81,15 +81,17 @@ int main( void )
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
 	// Projection matrix:45бу filed of view; 4:3 ratio; display range: 0.1 -100 units
-	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 6.0f, 100.0f);
+	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f);
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
 			glm::vec3(4, 3, 3),		// Camera is at (4, 3, 3) of World Space
 			glm::vec3(0, 0, 0),		// and looks at the origin
-			glm::vec3(0, 1, 0)		// Head is up(set to 0, -1, 0 to look upside-down)
+			glm::vec3(0, -1, 0)		// Head is up(set to 0, -1, 0 to look upside-down)
 		);
 	// Model matrix
 	glm::mat4 Model = glm::mat4(1.0f);
+	//Model = glm::translate(Model, glm::vec3(0, 0, 1));
 	// Our ModelViewProjection: multiplication of our 3 matrix
 	glm::mat4 MVP = Projection * View * Model;
 	//Get a handle for our "MVP" uniform
